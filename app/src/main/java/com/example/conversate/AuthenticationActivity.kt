43ba3dca5026d94c986a10c583314fe2f0d4da64
noah-erasmus <55457778+noah-erasmus@users.noreply.github.com>
2021-05-02 19:25:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/conversate/AuthenticationActivity.kt
@@ -0,0 +1,29 @@
+package com.example.conversate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.conversate.fragments.LoginFragment
+import com.example.conversate.fragments.SignupFragment
+import kotlinx.android.synthetic.main.fragment_signup.*
+
+class AuthenticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+
+        val signupFragment = SignupFragment()
+        val loginFragment = LoginFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.auth_fragment_area, signupFragment)
+            commit()
+        }
+
+        signup_btn.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.auth_fragment_area, signupFragment)
+                commit()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/conversate/fragments/SignupFragment.kt
@@ -0,0 +1,24 @@
+package com.example.conversate.fragments
+
+import android.os.Bundle
+import android.text.TextUtils.replace
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.conversate.R
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class SignupFragment : Fragment(R.layout.fragment_signup) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val loginFragment = LoginFragment()
+
+        login_btn.setOnClickListener{
+        }
+    }
+}
